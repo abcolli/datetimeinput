53b60ca838b9a7e2682d08771c49f955b1864fef
abcolli <59970520+abcolli@users.noreply.github.com>
2020-02-04 14:08:21 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('hi')
+from datetime import datetime
+expectedformat = '%Y-%m-%d %H:%M:%S'
+dt = input('Enter a valid date and time: ')
+try:
+    dt = datetime.strptime('2019-Jan-03 22:04:00', expectedformat)
+except ValueError:
+    print('Invalid datetime')
+print(dt)
